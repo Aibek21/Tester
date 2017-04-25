0f5ee7ebaa3fb310e07c4abfce3240183be38f3b
Aibek21 <aibekkuralbaev@gmail.com>
2017-04-25 17:07:38 +0600

    asd

--- Tester/settings.py
+++ Tester/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
